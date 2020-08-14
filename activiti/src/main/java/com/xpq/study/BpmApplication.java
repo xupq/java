28dc065fddb094f0128f9511b9471d4c6fc24c8e
xupengqian <857948134@qq.com>
2020-08-14 17:38:12 +0800

    init

---
+++ activiti/src/main/java/com/xpq/study/BpmApplication.java
@@ -0,0 +1,12 @@
+package com.xpq.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmApplication.class, args);
+    }
+}
---
+++ pay/src/main/java/com/xpq/study/ConsulApplication.java
@@ -0,0 +1,16 @@
+package com.xpq.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ConsulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulApplication.class, args);
+    }
+}
+
